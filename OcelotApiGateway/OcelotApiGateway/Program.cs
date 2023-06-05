3082ed142008b741f884d18497b5c72f9e4fac63
Andrew Ratushnyak <Andriy.Ratushniak@fivesysdev.com>
2023-06-05 15:54:15 +0300

    Add api gateway

--- OcelotApiGateway/OcelotApiGateway/Program.cs
+++ OcelotApiGateway/OcelotApiGateway/Program.cs
@@ -1,3 +1,6 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var ocelotConf = Environment.GetEnvironmentVariable("Ocelot_conf") ?? "local";
+
+builder.Configuration.AddJsonFile($"ocelot.{ocelotConf}.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
@@ -17,7 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseOcelot().Wait();
 app.UseAuthorization();
 
 app.MapControllers();
