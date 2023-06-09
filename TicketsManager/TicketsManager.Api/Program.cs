c6a0396533128416f94dec9be523a4669928352f
Andrew Ratushnyak <Andriy.Ratushniak@fivesysdev.com>
2023-06-09 12:53:38 +0300

    Add authorization by JWT

--- TicketsManager/TicketsManager.Api/Program.cs
+++ TicketsManager/TicketsManager.Api/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using System.Text.Json.Serialization;
 using TicketsManager.Api.Middlewares;
 using TicketsManager.BLL.Extensions;
@@ -21,11 +25,41 @@ app.Run();
 
 static void AddServices(WebApplicationBuilder builder)
 {
+    builder.Services.AddAuthorization();
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = JwtOptions.Issuer,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                IssuerSigningKey = JwtOptions.GetSymmetricSecurityKey(),
+                ValidateIssuerSigningKey = true,
+            };
+        });
+
     builder.Services.AddControllers().AddJsonOptions(x =>
         x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new() { Title = "Ellogy API", Version = "v1" });
+
+        c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        {
+            Description = "Standard Authorization header using Bearer scheme, e.g. \"bearer {token}\"",
+            In = ParameterLocation.Header,
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey
+        });
+
+        c.OperationFilter<SecurityRequirementsOperationFilter>();
+    });
+
     builder.Services.AddHealthChecks();
 
     builder.Services.AddDataLayer(EnvironmentVariables.ConnectionString);
@@ -43,6 +77,9 @@ static void AddMiddleware(WebApplication app)
 
     app.UseHttpsRedirection();
     app.UseHealthChecks("/health");
+
+    app.UseRouting();
+    app.UseAuthentication();
     app.UseAuthorization();
 
     app.UseMiddleware<ExceptionHandlerMiddleware>();
