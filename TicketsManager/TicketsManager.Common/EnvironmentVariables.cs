f7c36eab7600f101a162857f8559a1a6c1bd2e7f
Andrew Ratushnyak <Andriy.Ratushniak@fivesysdev.com>
2023-06-29 23:08:06 +0300

    Add exceptions to env variables to ticket manager

--- TicketsManager/TicketsManager.Common/EnvironmentVariables.cs
+++ TicketsManager/TicketsManager.Common/EnvironmentVariables.cs
@@ -1,8 +1,13 @@
-﻿namespace TicketsManager.Common
+﻿using TicketsManager.Common.Exceptions;
+
+namespace TicketsManager.Common
 {
     public static class EnvironmentVariables
     {
-        public static readonly string? ConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
-        public static readonly string? JwtSecretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
+        public static readonly string? ConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+                                                          ?? throw new EnvironmentVariableNotFoundException("CONNECTION_STRING");
+		
+        public static readonly string? JwtSecretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY")
+                                                      ?? throw new EnvironmentVariableNotFoundException("JWT_SECRET_KEY");
     }
 }
