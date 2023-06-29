9272a420d02582c0e50f58b04031d6326b0bfff8
Andrew Ratushnyak <Andriy.Ratushniak@fivesysdev.com>
2023-06-29 23:22:05 +0300

    Change connection string

--- TicketsManager/TicketsManager.DAL/Extensions/DiExtension.cs
+++ TicketsManager/TicketsManager.DAL/Extensions/DiExtension.cs
@@ -8,7 +8,7 @@ namespace TicketsManager.DAL.Extensions;
 
 public static class DiExtension
 {
-    public static IServiceCollection AddDataLayer(this IServiceCollection services, string? connectionString)
+    public static IServiceCollection AddDataLayer(this IServiceCollection services, string connectionString)
     {
         if (string.IsNullOrEmpty(connectionString))
             throw new ArgumentNullException(nameof(connectionString));
