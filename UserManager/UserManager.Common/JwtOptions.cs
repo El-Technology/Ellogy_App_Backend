10aa42ed2805ae55db834671edf4a712cf3e6fe6
Andrew Ratushnyak <Andriy.Ratushniak@fivesysdev.com>
2023-07-03 09:31:38 +0300

    Change token ttl

--- UserManager/UserManager.Common/JwtOptions.cs
+++ UserManager/UserManager.Common/JwtOptions.cs
@@ -6,7 +6,7 @@ namespace UserManager.Common;
 public static class JwtOptions
 {
     public const string Issuer = "https://ellogy.usermanager";
-    public static readonly TimeSpan TokenLifeTime = TimeSpan.FromMinutes(10);
+    public static readonly TimeSpan TokenLifeTime = TimeSpan.FromMinutes(100);
 
     private static readonly string? Key = EnvironmentVariables.JwtSecretKey;
 
