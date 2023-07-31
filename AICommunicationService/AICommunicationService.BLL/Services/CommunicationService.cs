a7e4f1f48ff1cb8b06b3ae1f4ac2df2f1ac0e207
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2023-07-31 12:59:48 +0300

    Basic structure for Ai communication service

---
+++ AICommunicationService/AICommunicationService.BLL/Services/CommunicationService.cs
@@ -0,0 +1,22 @@
+﻿using AICommunicationService.BLL.Interfaces;
+using OpenAI_API;
+
+namespace AICommunicationService.BLL.Services
+{
+    public class CommunicationService : ICommunicationService
+    {
+        private readonly OpenAIAPI _openAIAPI;
+        public CommunicationService(OpenAIAPI openAIAPI)
+        {
+            _openAIAPI = openAIAPI;
+        }
+
+        public async Task<string> SendMessageAsync(string message)
+        {
+            var result = await _openAIAPI.Completions.GetCompletion(message);
+
+
+            return result;
+        }
+    }
+}
