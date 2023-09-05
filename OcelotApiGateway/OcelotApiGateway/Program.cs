1cdaf474728ebd0353b6fa804416a10c808156d9
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2023-09-05 11:36:10 +0300

    update ocelot

--- OcelotApiGateway/OcelotApiGateway/Program.cs
+++ OcelotApiGateway/OcelotApiGateway/Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddCors(options =>
     {
         builder.AllowAnyOrigin()
             .AllowAnyHeader()
-            .AllowAnyMethod();
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
 
