8be6a05cc9bd1f0860067c96696e390dd1a4c5d0
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2023-09-27 15:21:30 +0300

    Update CreateConversationRequest.cs

--- AICommunicationService/AICommunicationService.Common/Models/AIRequest/CreateConversationRequest.cs
+++ AICommunicationService/AICommunicationService.Common/Models/AIRequest/CreateConversationRequest.cs
@@ -1,9 +1,11 @@
 ﻿using AICommunicationService.Common.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace AICommunicationService.Common.Models.AIRequest
 {
     public class CreateConversationRequest
     {
+        [Required]
         public AiModelEnum AiModelEnum { get; set; }
         public string TemplateName { get; set; }
         public string UserInput { get; set; }
