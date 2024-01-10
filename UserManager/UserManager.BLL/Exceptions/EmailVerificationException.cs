a5a59709bdd4b13daac611745b8246b5e47b8c5d
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-01-10 12:10:33 +0200

    Add custom exception for emai verification

---
+++ UserManager/UserManager.BLL/Exceptions/EmailVerificationException.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManager.BLL.Exceptions
+{
+    public class EmailVerificationException : Exception
+    {
+        public new string Message { get; set; } = "You need to activate your account";
+        public EmailVerificationException()
+        { }
+    }
+}
