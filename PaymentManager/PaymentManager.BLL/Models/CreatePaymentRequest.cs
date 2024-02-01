6fe1895525f71f33016f0f817f5cf949343b8f44
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-02-01 12:17:55 +0200

    Code clean up

--- PaymentManager/PaymentManager.BLL/Models/CreatePaymentRequest.cs
+++ PaymentManager/PaymentManager.BLL/Models/CreatePaymentRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace PaymentManager.BLL.Models
 {
-    public class CreatePaymentRequest : CreateSessionRequest    
+    public class CreatePaymentRequest : CreateSessionRequest
     {
         public int AmountOfPoints { get; set; }
     }
