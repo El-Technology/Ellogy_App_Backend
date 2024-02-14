1198eb2605f62ddf3de86530496844dbe7c33569
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-02-14 13:10:51 +0200

    Update migration record

---
+++ PaymentManager/PaymentManager.DAL/Migrations/20240214110230_RemoveUserTable.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentManager.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
