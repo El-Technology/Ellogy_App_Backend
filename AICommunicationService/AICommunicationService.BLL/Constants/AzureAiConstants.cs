b8bc3ded85e250e7790d32f82393f3c24dcb6982
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-03-08 11:57:58 +0200

    Update AzureAiConstants.cs

--- AICommunicationService/AICommunicationService.BLL/Constants/AzureAiConstants.cs
+++ AICommunicationService/AICommunicationService.BLL/Constants/AzureAiConstants.cs
@@ -1,11 +1,10 @@
-﻿namespace AICommunicationService.BLL.Constants
+﻿namespace AICommunicationService.BLL.Constants;
+
+public static class AzureAiConstants
 {
-    public static class AzureAiConstants
-    {
-        public const string TurboModel = "EllogyGptTurbo";
-        public const string FourModel = "EllogyGptFour";
-        public const string ResourceName = "EllogyCommunication";
-        public const string ApiVersion = "api-version=2023-07-01-preview";
-        public const string BaseUrl = "https://ellogycommunication.openai.azure.com/openai/deployments/";
-    }
+    public const string TurboModel = "EllogyGptTurbo";
+    public const string FourModel = "EllogyGptFour";
+    public const string ResourceName = "EllogyCommunication";
+    public const string ApiVersion = "api-version=2024-02-15-preview";
+    public const string BaseUrl = "https://ellogycommunication.openai.azure.com/openai/deployments/";
 }
