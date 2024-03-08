eaae561bdccdcd3fca04166e65ec6143b578d35f
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-03-08 11:44:43 +0200

    Update api-version for azure open ai, and change embedding model on a newest one

--- AICommunicationService/AICommunicationService.BLL/Constants/AzureAiConstants.cs
+++ AICommunicationService/AICommunicationService.BLL/Constants/AzureAiConstants.cs
@@ -1,14 +1,13 @@
-﻿namespace AICommunicationService.BLL.Constants
+﻿namespace AICommunicationService.BLL.Constants;
+
+public static class AzureAiConstants
 {
-    public static class AzureAiConstants
-    {
-        public const string TurboModel = "EllogyGptTurbo";
-        public const string FourModel = "EllogyGptFour";
-        public const string FourTurboModel = "EllogyGptFourTurbo";
-        public const string Four32kModel = "EllogyGptFour32k";
-        public const string ResourceName = "EllogyCommunication";
-        public const string EmbeddingUrl = "https://ellogycommunication.openai.azure.com/openai/deployments/EllogyEmbedding/embeddings?api-version=2023-05-15";
-        public const string ApiVersion = "api-version=2023-07-01-preview";
-        public const string BaseUrl = "https://ellogycommunication.openai.azure.com/openai/deployments/";
-    }
+    public const string TurboModel = "EllogyGptTurbo";
+    public const string FourModel = "EllogyGptFour";
+    public const string FourTurboModel = "EllogyGptFourTurbo";
+    public const string Four32kModel = "EllogyGptFour32k";
+    public const string ResourceName = "EllogyCommunication";
+    public const string EmbeddingUrl = $"https://ellogycommunication.openai.azure.com/openai/deployments/EllogyEmbedding3Small/embeddings?{ApiVersion}";
+    public const string ApiVersion = "api-version=2024-02-15-preview";
+    public const string BaseUrl = "https://ellogycommunication.openai.azure.com/openai/deployments/";
 }
