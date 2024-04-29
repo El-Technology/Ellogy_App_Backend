cd86964f8eccdff7ca142b9861efa0dc9232aa09
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-04-29 13:32:28 +0300

    Update configure config

---
+++ UserManager/NotificationService/EnvironmentVariables.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using UserManager.Common.Constants;
+
+namespace NotificationService;
+
+public static class EnvironmentVariables
+{
+    private static readonly Lazy<Task<ConcurrentDictionary<string, string>>> _secrets = new(async () =>
+    {
+        var vaultUri = new Uri(Environment.GetEnvironmentVariable(ConfigConstants.KeyVaultStorageUrl));
+        var client = new SecretClient(vaultUri, new DefaultAzureCredential());
+        var secretsDictionary = new ConcurrentDictionary<string, string>();
+
+        await Task.WhenAll(
+            GetAndAddSecretAsync(client, SecretNames.MailFrom, secretsDictionary),
+            GetAndAddSecretAsync(client, SecretNames.EmailClientConnectionString, secretsDictionary),
+            GetAndAddSecretAsync(client, SecretNames.BlobStorageConnectionString, secretsDictionary)
+        );
+
+        return secretsDictionary;
+    });
+
+    private static async Task GetAndAddSecretAsync(SecretClient client, string secretName, ConcurrentDictionary<string, string> secretsDictionary)
+    {
+        var secret = await client.GetSecretAsync(secretName);
+        secretsDictionary.TryAdd(secretName, secret.Value.Value);
+    }
+
+    public static async Task<string> GetSecretAsync(string key)
+    {
+        var secrets = await _secrets.Value;
+        if (!secrets.ContainsKey(key))
+        {
+            throw new ArgumentException($"Secret with key '{key}' not found");
+        }
+
+        return secrets[key];
+    }
+    public static Task<string> MailFrom => GetSecretAsync(SecretNames.MailFrom);
+    public static Task<string> EmailClientConnectionString => GetSecretAsync(SecretNames.EmailClientConnectionString);
+    public static Task<string> BlobStorageConnectionString => GetSecretAsync(SecretNames.BlobStorageConnectionString);
+}
--- UserManager/NotificationService/Startup.cs
+++ UserManager/NotificationService/Startup.cs
@@ -1,13 +1,10 @@
 ﻿using Azure.Communication.Email;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NotificationService.Interfaces;
 using NotificationService.Services;
-using System.IO;
 using System.Threading.Tasks;
-using UserManager.Common;
 using UserManager.Common.Constants;
 
 [assembly: FunctionsStartup(typeof(NotificationService.Startup))]
@@ -15,16 +12,6 @@ namespace NotificationService;
 
 public class Startup : FunctionsStartup
 {
-    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
-    {
-        var context = builder.GetContext();
-
-        builder.ConfigurationBuilder
-            .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
-            .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{context.EnvironmentName}.json"), optional: true, reloadOnChange: false)
-            .AddEnvironmentVariables();
-    }
-
     public override void Configure(IFunctionsHostBuilder builder)
     {
         ConfigureServicesAsync(builder).GetAwaiter().GetResult();
