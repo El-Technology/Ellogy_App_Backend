fcb16bd5bc88ee72c5eaceb9413af814cf7f3cd3
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-05-02 16:32:02 +0300

    Add autofill AiPrompts table in database

---
+++ AICommunicationService/AICommunicationService.Common/Helpers/DbSeedHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace AICommunicationService.Common.Helpers;
+public static class DbSeedHelper
+{
+    public static T? ConvertJsonToList<T>(string path, string projectName)
+    {
+        var fullPath = Path.Combine(Directory.GetCurrentDirectory(), path);
+        var jsonText = File.ReadAllText(fullPath.Replace(".Api", projectName));
+
+        return JsonSerializer.Deserialize<T>(jsonText);
+    }
+}
---
+++ AICommunicationService/AICommunicationService.DAL/Models/Root/AIPromptsRoot.cs
@@ -0,0 +1,5 @@
+﻿namespace AICommunicationService.DAL.Models.Root;
+public class AIPromptsRoot
+{
+    public List<AIPrompt> AIPrompts { get; set; } = new List<AIPrompt>();
+}
