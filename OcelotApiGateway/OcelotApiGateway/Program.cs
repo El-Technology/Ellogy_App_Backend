c1dc58e1916641c34b2466e159da3b176a140a80
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-05-08 14:26:21 +0300

    Test SSE streaming

--- OcelotApiGateway/OcelotApiGateway/Program.cs
+++ OcelotApiGateway/OcelotApiGateway/Program.cs
@@ -1,6 +1,8 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Polly;
+using System.Text;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,8 +36,54 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
 app.UseHttpsRedirection();
 app.MapControllers();
+
+app.MapPost("dev/Communication/getStreamResponse", async context =>
+{
+    context.Response.Headers.Add("Content-Type", "text/event-stream");
+
+    var requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
+    var jsonRequest = JsonSerializer.Serialize(requestBody);
+    var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+    using var httpClient = new HttpClient();
+
+    foreach (var header in context.Request.Headers)
+    {
+        if (header.Key != "Content-Type"
+            && header.Key != "Host"
+            && header.Key != "Connection"
+            && header.Key != "Content-Length")
+        {
+            httpClient.DefaultRequestHeaders.Add(header.Key, header.Value.ToString());
+        }
+    }
+
+    using var response = await httpClient.PostAsync(
+        "http://20.21.129.55:53053/api/Communication/getStreamResponse", content);
+    using var stream = await response.Content.ReadAsStreamAsync();
+
+    if (!response.IsSuccessStatusCode)
+    {
+        context.Response.StatusCode = (int)response.StatusCode;
+        await stream.CopyToAsync(context.Response.Body);
+        return;
+    }
+
+    using var reader = new StreamReader(stream);
+    while (!reader.EndOfStream)
+    {
+        var line = await reader.ReadLineAsync();
+        await context.Response.WriteAsync(line ?? string.Empty);
+    }
+});
 
 app.UseCors();
 app.UseWebSockets();
