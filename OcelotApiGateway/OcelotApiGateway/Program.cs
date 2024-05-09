1ccdc4750d3f10dd6552e7d277ab70dd53f7e366
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-05-09 11:25:30 +0300

    Add SSE communication endpoint

--- OcelotApiGateway/OcelotApiGateway/Program.cs
+++ OcelotApiGateway/OcelotApiGateway/Program.cs
@@ -39,8 +39,60 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
 app.UseHttpsRedirection();
 app.MapControllers();
+
+app.MapPost("dev/Communication/getStreamResponse", async context =>
+{
+    context.Response.Headers.Add("Content-Type", "text/event-stream");
+
+    var requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
+    var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+    using var httpClient = new HttpClient();
+
+    foreach (var header in context.Request.Headers)
+    {
+        if (header.Key != "Content-Type"
+            && header.Key != "Host"
+            && header.Key != "Connection"
+            && header.Key != "Content-Length")
+        {
+            httpClient.DefaultRequestHeaders.Add(header.Key, header.Value.ToString());
+        }
+    }
+
+    var message = new HttpRequestMessage
+    {
+        RequestUri = new Uri(
+            $"http://{Environment.GetEnvironmentVariable("SSH_HOST")}:53053/api/Communication/getStreamResponse"),
+        Method = HttpMethod.Post,
+        Content = content
+    };
+
+    var response = await httpClient.SendAsync(message, HttpCompletionOption.ResponseHeadersRead);
+
+    using var stream = await response.Content.ReadAsStreamAsync();
+    if (!response.IsSuccessStatusCode)
+    {
+        context.Response.StatusCode = (int)response.StatusCode;
+        await stream.CopyToAsync(context.Response.Body);
+        return;
+    }
+
+    using var reader = new StreamReader(stream);
+    while (!reader.EndOfStream)
+    {
+        await context.Response.WriteAsync($"{await reader.ReadLineAsync()}\n");
+        await context.Response.Body.FlushAsync();
+    }
+});
 
 app.UseCors();
 app.UseWebSockets();
