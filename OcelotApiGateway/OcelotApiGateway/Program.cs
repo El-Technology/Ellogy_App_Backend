cc9b9a8c94cc6e4bceba7268198704d3c299ab9f
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-05-09 10:07:42 +0300

    Add custom endpoint for handling SSE response

--- OcelotApiGateway/OcelotApiGateway/Program.cs
+++ OcelotApiGateway/OcelotApiGateway/Program.cs
@@ -2,7 +2,6 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Polly;
 using System.Text;
-using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -50,7 +49,6 @@ app.MapPost("dev/Communication/getStreamResponse", async context =>
     context.Response.Headers.Add("Content-Type", "text/event-stream");
 
     var requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
-    var jsonRequest = JsonSerializer.Serialize(requestBody);
     var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
 
     using var httpClient = new HttpClient();
@@ -66,10 +64,16 @@ app.MapPost("dev/Communication/getStreamResponse", async context =>
         }
     }
 
-    using var response = await httpClient.PostAsync(
-        "http://20.21.129.55:53053/api/Communication/getStreamResponse", content);
-    using var stream = await response.Content.ReadAsStreamAsync();
+    var message = new HttpRequestMessage
+    {
+        RequestUri = new Uri("http://20.21.129.55:53053/api/Communication/getStreamResponse"),
+        Method = HttpMethod.Post,
+        Content = content
+    };
 
+    var response = await httpClient.SendAsync(message, HttpCompletionOption.ResponseHeadersRead);
+
+    using var stream = await response.Content.ReadAsStreamAsync();
     if (!response.IsSuccessStatusCode)
     {
         context.Response.StatusCode = (int)response.StatusCode;
@@ -80,8 +84,8 @@ app.MapPost("dev/Communication/getStreamResponse", async context =>
     using var reader = new StreamReader(stream);
     while (!reader.EndOfStream)
     {
-        var line = await reader.ReadLineAsync();
-        await context.Response.WriteAsync(line ?? string.Empty);
+        await context.Response.WriteAsync($"{await reader.ReadLineAsync()}\n");
+        await context.Response.Body.FlushAsync();
     }
 });
 
