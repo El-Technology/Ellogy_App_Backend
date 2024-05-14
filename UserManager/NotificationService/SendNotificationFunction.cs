b61ff34d2a1003b83738b0be5fd8abc38c1e2bb9
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-05-14 14:54:58 +0300

    Add test endpoint

--- UserManager/NotificationService/SendNotificationFunction.cs
+++ UserManager/NotificationService/SendNotificationFunction.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Newtonsoft.Json;
 using NotificationService.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UserManager.Common;
 using UserManager.Common.Models.NotificationModels;
@@ -28,5 +32,21 @@ public class SendNotificationFunction
         catch (Exception)
         {
         }
+    }
+
+    [FunctionName("HttpFunction")]
+    public async Task<IActionResult> Run(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "projectStarted")] HttpRequest req)
+    {
+        await _notifier.SendNotificationAsync(new NotificationModel
+        {
+            Type = NotificationTypeEnum.VerifyEmail,
+            Way = NotificationWayEnum.Email,
+            MetaData = new Dictionary<string, string>
+            { { "{{{firstName}}}", "sharkovskiy1@gmail.com" }, { "{{{verifyEmailAddressLink}}}", "asd" } },
+            Consumer = "sharkovskiy1@gmail.com"
+        });
+
+        return new OkResult();
     }
 }
