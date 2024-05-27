90851248f80d137b636cee816f7553b19d329b84
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-05-27 12:42:38 +0300

    Create EmailHelper.cs

---
+++ UserManager/UserManager.Common/Helpers/EmailHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserManager.Common.Helpers;
+public static class EmailHelper
+{
+    private static readonly Regex EmailRegex = new Regex(
+    @"^(([^<>()\[\]\\.,;:\s@\""]+(\.[^<>()\[\]\\.,;:\s@\""]+)*)|("".+""))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z0-9]+[-a-zA-Z0-9]*[a-zA-Z0-9]+\.)+[a-zA-Z]{2,}))$",
+    RegexOptions.Compiled);
+
+    public static bool IsValidEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            return false;
+        }
+
+        return EmailRegex.IsMatch(email);
+    }
+}
