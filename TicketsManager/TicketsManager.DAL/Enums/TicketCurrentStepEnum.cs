a450bf12b18c41bb86e977a32054dff22df3a58e
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-06-06 07:37:43 +0300

    Update TicketCurrentStepEnum.cs

--- TicketsManager/TicketsManager.DAL/Enums/TicketCurrentStepEnum.cs
+++ TicketsManager/TicketsManager.DAL/Enums/TicketCurrentStepEnum.cs
@@ -1,10 +1,10 @@
-﻿namespace TicketsManager.DAL.Enums
+﻿namespace TicketsManager.DAL.Enums;
+
+public enum TicketCurrentStepEnum
 {
-    public enum TicketCurrentStepEnum
-    {
-        General = 0,
-        Usecases = 1,
-        Notifications = 2,
-        Report = 3
-    }
+    General = 0,
+    Usecases = 1,
+    TestCases = 2,
+    Notifications = 3,
+    Report = 4
 }
