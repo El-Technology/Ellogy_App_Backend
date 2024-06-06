22c125cd11a76874c6bda049c670ff8f03f02641
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-06-06 09:01:51 +0300

    Create TemplatesWithoutChargeConst.cs

---
+++ AICommunicationService/AICommunicationService.BLL/Constants/TemplatesWithoutChargeConst.cs
@@ -0,0 +1,14 @@
+﻿namespace AICommunicationService.BLL.Constants;
+public class TemplatesWithoutChargeConst
+{
+    public static IEnumerable<string> ListOfFreeTemplates = new List<string>
+    {
+        RegenerateUseCaseDiagramTemplate,
+        RegenerateSequenceDiagramTemplate,
+        RegenerateFlowchartTemplate
+    };
+
+    public const string RegenerateUseCaseDiagramTemplate = "RegenerateUseCaseDiagramTemplate";
+    public const string RegenerateSequenceDiagramTemplate = "RegenerateSequenceDiagramTemplate";
+    public const string RegenerateFlowchartTemplate = "RegenerateFlowchartTemplate";
+}
