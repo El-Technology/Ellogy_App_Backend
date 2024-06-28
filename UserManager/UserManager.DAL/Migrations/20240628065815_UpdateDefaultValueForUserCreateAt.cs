aea8677a81f90fd9930d6883ea1df885c0a92b5a
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-06-28 10:31:46 +0300

    Update default value of CreateAt

---
+++ UserManager/UserManager.DAL/Migrations/20240628065815_UpdateDefaultValueForUserCreateAt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManager.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDefaultValueForUserCreateAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW() AT TIME ZONE 'UTC'",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW() AT TIME ZONE 'UTC'");
+        }
+    }
+}
