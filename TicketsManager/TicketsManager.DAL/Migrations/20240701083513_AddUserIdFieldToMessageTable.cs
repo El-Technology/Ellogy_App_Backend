c4a773b1c131eae16291f83055b6edf5b4462a29
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-07-01 12:05:25 +0300

    Add userId field to messages with autofillup

---
+++ TicketsManager/TicketsManager.DAL/Migrations/20240701083513_AddUserIdFieldToMessageTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketsManager.DAL.Migrations;
+
+/// <inheritdoc />
+public partial class AddUserIdFieldToMessageTable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<Guid>(
+            name: "UserId",
+            table: "Messages",
+            type: "uuid",
+            nullable: true);
+
+        migrationBuilder.Sql("UPDATE \"Messages\" ms\r\nSET \"UserId\" = (SELECT \"UserId\" FROM \"Tickets\" ts WHERE ms.\"TicketId\" = ts.\"Id\")");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "UserId",
+            table: "Messages");
+    }
+}
