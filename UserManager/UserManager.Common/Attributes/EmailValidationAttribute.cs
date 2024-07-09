3d461bdf06f2730f72e077ac50a6e160752ae4b6
Vladislav Sharkovskyi <Vladislav.Sharkovskyi@fivesysdev.com>
2024-07-09 14:12:29 +0300

    Attributes configuration

---
+++ UserManager/UserManager.Common/Attributes/EmailValidationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UserManager.Common.Exceptions;
+using UserManager.Common.Helpers;
+
+namespace UserManager.Common.Attributes;
+public class EmailValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is string email)
+        {
+
+            if (!EmailHelper.IsValidEmail(email))
+                throw new InvalidEmailException(email);
+
+            return ValidationResult.Success;
+        }
+        return new ValidationResult("Email is required");
+    }
+}
---
+++ UserManager/UserManager.Common/Attributes/PasswordValidationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UserManager.Common.Helpers;
+
+namespace UserManager.Common.Attributes;
+public class PasswordValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is string password)
+        {
+            PasswordValidationHelper.ValidatePassword(password);
+
+            return ValidationResult.Success;
+        }
+        return new ValidationResult("Password is required");
+    }
+}
